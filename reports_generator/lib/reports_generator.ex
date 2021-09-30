7648702a46f9e8fd6b10aec3ae30fe10621ac998
vineboneto <vineboneto@gmail.com>
2021-09-29 23:50:37 -0300

    feat: usando Enum.reduct

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,5 +1,23 @@
 defmodule ReportsGenerator do
   def build(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.reduce(report_acc(), fn line, report ->
+      [id, _food_name, price] = parser_line(line)
+      Map.put(report, id, report[id] + price)
+    end)
+  end
+
+  defp parser_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+
+  defp report_acc(), do: Enum.into(1..30, %{}, fn elem -> {Integer.to_string(elem), 0} end)
+
+  def build_2(filename) do
     case File.read("reports/#{filename}") do
       {:ok, result} ->
         result
@@ -11,7 +29,7 @@ defmodule ReportsGenerator do
     end
   end
 
-  def build_2(filename) do
+  def build_3(filename) do
     "reports/#{filename}"
     |> File.read()
     |> handle_file()
