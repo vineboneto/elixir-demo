aa47c55e43b0ca5fea361ca401706434c6d3261e
vineboneto <vineboneto@gmail.com>
2021-09-30 10:57:47 -0300

    feat: criado modulo parser

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,40 +1,34 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), fn line, report ->
-      [id, _food_name, price] = parser_line(line)
-      Map.put(report, id, report[id] + price)
-    end)
+    filename
+    |> Parser.parser_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  defp parser_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
-  end
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc(), do: Enum.into(1..30, %{}, fn elem -> {Integer.to_string(elem), 0} end)
 
-  def build_2(filename) do
-    case File.read("reports/#{filename}") do
-      {:ok, result} ->
-        result
+  # def build_2(filename) do
+  #   case File.read("reports/#{filename}") do
+  #     {:ok, result} ->
+  #       result
 
-      {:error, reason} ->
-        reason
-        # Default
-        # _ -> "caso qualquer"
-    end
-  end
+  #     {:error, reason} ->
+  #       reason
+  #       # Default
+  #       # _ -> "caso qualquer"
+  #   end
+  # end
 
-  def build_3(filename) do
-    "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
-  end
+  # def build_3(filename) do
+  #   "reports/#{filename}"
+  #   |> File.read()
+  #   |> handle_file()
+  # end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while open file!"
+  # defp handle_file({:ok, file_content}), do: file_content
+  # defp handle_file({:error, _reason}), do: "Error while open file!"
 end
