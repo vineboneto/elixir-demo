f1bb003bd64ce08af9d00e7ae667828a3ffb856a
vineboneto <vineboneto@gmail.com>
2022-06-13 21:57:16 -0300

    feat: structs

---
+++ exlivery/lib/orders/item.ex
@@ -0,0 +1,30 @@
+defmodule Exlivery.Orders.Item do
+  @categories [:pizza, :hamburguer, :carne, :prato_feito, :japonesa, :sobremesa]
+
+  @keys [:description, :category, :unity_price, :quantity]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(description, category, unit_price, quantity)
+      when quantity > 0 and category in @categories do
+    unit_price
+    |> Decimal.cast()
+    |> build_item(description, category, quantity)
+  end
+
+  def build(_description, _category, _unit_price, _quantity), do: {:error, "Invalid parameters"}
+
+  defp build_item({:ok, unity_price}, description, category, quantity) do
+    {:ok,
+     %__MODULE__{
+       description: description,
+       category: category,
+       unity_price: unity_price,
+       quantity: quantity
+     }}
+  end
+
+  defp build_item(:error, _description, _category, _quantity),
+    do: {:error, "Invalid parameters"}
+end
--- exlivery/lib/users/user.ex
+++ exlivery/lib/users/user.ex
@@ -1,18 +1,19 @@
 defmodule Exlivery.Users.User do
-
-  @keys  [:name, :email, :cpf, :age]
+  @keys [:name, :email, :cpf, :age, :address]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, age, cpf) when age >= 18 and is_bitstring(cpf) do
-    {:ok, %__MODULE__{
-      name: name,
-      email: email,
-      cpf: cpf,
-      age: age,
-    }}
+  def build(name, email, age, cpf, address) when age >= 18 and is_bitstring(cpf) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       cpf: cpf,
+       age: age,
+       address: address
+     }}
   end
 
-  def build(_name, _email, _age, _cpf), do: {:error, "Invalid parameters"}
+  def build(_name, _email, _age, _cpf, _address), do: {:error, "Invalid parameters"}
 end
