407282ceebc919b9734c21a5e830f08f5dcc6a30
vineboneto <vineboneto@gmail.com>
2022-06-14 17:07:11 -0300

    feat: orders

---
+++ exlivery/lib/orders/agent.ex
@@ -0,0 +1,30 @@
+defmodule Exlivery.Orders.Agent do
+  alias Exlivery.Orders.Order
+
+  use Agent
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%Order{} = order) do
+    uuid = UUID.uuid4()
+    Agent.update(__MODULE__, fn state -> update_state(state, order, uuid) end)
+    {:ok, uuid}
+  end
+
+  def get(uuid) do
+    Agent.get(__MODULE__, fn state -> get_order(state, uuid) end)
+  end
+
+  defp get_order(state, uuid) do
+    case Map.get(state, uuid) do
+      nil -> {:error, "Order not found"}
+      order -> {:ok, order}
+    end
+  end
+
+  defp update_state(state, %Order{} = order, uuid) do
+    Map.put(state, uuid, order)
+  end
+end
---
+++ exlivery/lib/users/create_or_update.ex
@@ -0,0 +1,19 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users
+  alias Users.Agent, as: UserAgent
+  alias Users.User
+
+  def call(%{name: name, email: email, age: age, cpf: cpf, address: address}) do
+    name
+    |> User.build(email, age, cpf, address)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}) do
+    UserAgent.save(user)
+  end
+
+  defp save_user({:error, _reason} = error) do
+    error
+  end
+end
