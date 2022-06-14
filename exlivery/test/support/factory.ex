78bb4848959656779363be2248dff2f56995c887
vineboneto <vineboneto@gmail.com>
2022-06-13 22:40:15 -0300

    test: valid tests order

--- exlivery/test/support/factory.ex
+++ exlivery/test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       description: "Sorvete",
       quantity: 20,
       unity_price: Decimal.new("10.50")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua John Doe",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("251.00"),
+      user_cpf: "12345"
     }
   end
 end
